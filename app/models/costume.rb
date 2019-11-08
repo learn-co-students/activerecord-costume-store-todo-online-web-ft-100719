78e36243561f31eb438e357bc999f7010e089ca7
github ginasekhar <github ginasekhar@yahoo.com>
2019-11-08 09:09:44 -0500

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,4 @@
-# Create your Costume class here
+class Costume < ActiveRecord::Base
+
+end# Create your CostumeStore class hereCreate your Costume class here
 # It should inherit from ActiveRecord::Base
