11224d33f92aaceb69692cd0c8271764b29277d5
Huda Yousif <hjoski@gmail.com>
2019-11-10 14:02:43 -0800

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,30 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.integer :price
+            t.string :size
+            t.string :image_url
+            t.datetime :created_at
+            t.datetime :updated_at
+        end
+    end
+end
+
+# sql = <<-SQL
+# CREATE TABLE IF NOT EXISTS costumes (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT,
+#     price INTEGER,
+#     size TEXT,
+#     image_url TEXT,
+#     created_at DATETIME,
+#     updated_at DATETIME
+# )
+# SQL
+
+
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
